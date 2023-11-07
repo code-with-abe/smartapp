9f779194ee4ee3b467f02da975ee5c6477a38dc8
code-with-abe <abraham.pabbathi@gmail.com>
2023-11-06 22:26:39 -0600

    added picture

--- smartapp/settings.py
+++ smartapp/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'bsmart/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'bsmart/static')]
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
