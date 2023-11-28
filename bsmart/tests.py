28bbb877a32622fd6f51425759a782deff41474e
code-with-abe <abraham.pabbathi@gmail.com>
2023-11-27 18:50:52 -0600

    added azure

--- bsmart/tests.py
+++ bsmart/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from azure.storage.blob import ContainerClient
 
-# Create your tests here.
+container_url = "https://sdasstorageeast02.blob.core.windows.net/raw?sp=racwdl&st=2023-11-17T04:46:51Z&se=2023-11-18T12:46:51Z&spr=https&sv=2022-11-02&sr=c&sig=SzSpr9s9lJ2SP8uAJR4Iyv6ZMwKVxto1T6mMv7AZ3QY%3D"
+container_client = ContainerClient.from_container_url(container_url)
+
+# List the blobs in the container
+# blob_list = container_client.list_blobs()
+# for blob in blob_list:
+#     print(blob.name)
+
+local_file_path = "../media/pickup.csv"
+blob_name = "diabetes/uploadpickup.csv"  # The name you want the blob to have in the container
+
+# Get a BlobClient to represent the blob we are creating
+blob_client = container_client.get_blob_client(blob=blob_name)
+
+# Upload the local file to Azure Storage
+with open(local_file_path, "rb") as data:
+    blob_client.upload_blob(data, overwrite=True)
--- bsmart/views.py
+++ bsmart/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from django.conf import settings
 
 from .process_file import process_csv
 
@@ -41,5 +43,33 @@ def upload(request):
         with open('media/' + uploaded_file.name, 'wb+') as destination:
             for chunk in uploaded_file.chunks():
                 destination.write(chunk)
-        return render(request, 'dashboard.html')
+                
+        if upload_to_adls(uploaded_file.name):
+            return render(request, 'dashboard.html')
+        else:
+            # Return an error response
+            return HttpResponse('Failed to upload file', status=500)
     return render(request, 'upload.html')
+
+# Initialize Azure Blob Service Client with your connection string
+
+container_url = settings.AZURE_CONTAINER_SAS
+
+def upload_to_adls(uploaded_file_name):
+    try:
+
+        container_client = ContainerClient.from_container_url(container_url)
+        
+        blob_name = "diabetes/bsmart_uploads/"+uploaded_file_name
+        # Upload the file to Azure storage
+        # Get a BlobClient to represent the blob we are creating
+        blob_client = container_client.get_blob_client(blob=blob_name)
+        with open('media/' + uploaded_file_name, "rb") as data:
+            blob_client.upload_blob(data, overwrite=True)
+
+        # Return True if the file has been uploaded successfully
+        return True
+    except Exception as e:
+        # Handle exceptions
+        print(e)
+        return False
